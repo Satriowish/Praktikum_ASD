3cb917d11ea5f9aa64d55f11931fd58d492a3a99
Satriowish <satriowisnuap@gmail.com>
2024-02-21 14:18:58 +0700

    P1_Praktikum2_Pertanyaan1_1F_26

---
+++ PrakASD_1F_26/src/P1/Perulangan_26.java
@@ -0,0 +1,32 @@
+package P1;
+
+import java.util.Scanner;
+
+public class Perulangan_26 {
+    public static void main(String[] args) {
+        Scanner input26 = new Scanner(System.in);
+        System.out.print("Input NIM: ");
+        String nim = input26.nextLine();
+        // untuk mengambil 2 digit angka terakhir dari NIM
+        int n = Integer.parseInt(nim.substring(nim.length() - 2));
+        // Jika n < 10, tambahkan 10
+        if (n < 10) {
+            n += 10;
+        }
+
+        System.out.print("OUTPUT : ");
+        for (int i = 1; i <= n; i++) {
+            if (i == 6 || i == 10) {
+                // Angka 6 dan 10 tidak dicetak
+                continue;
+            }
+            if (i % 2 == 0) {
+                // Angka genap dicetak sesuai bilangan aslinya
+                System.out.print(i + " ");
+            } else {
+                // Angka ganjil dicetak dengan asterik
+                System.out.print("* ");
+            }
+        }
+    }
+}
