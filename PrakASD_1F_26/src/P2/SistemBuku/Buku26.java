5814635338bd993d46b72a727800f2575a1188a8
Satriowish <satriowisnuap@gmail.com>
2024-02-28 16:50:59 +0700

    P2_Latihan1_1F_26

--- PrakASD_1F_26/src/P2/SistemBuku/Buku26.java
+++ PrakASD_1F_26/src/P2/SistemBuku/Buku26.java
@@ -7,6 +7,18 @@ public class Buku26 {
     int stok;
     int harga;
 
+    public Buku26() {
+
+    }
+
+    public Buku26(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+
     void tampilkanInformasi() {
         System.out.println();
         System.out.println("Judul          : " + judul);
@@ -32,15 +44,21 @@ public class Buku26 {
         harga = hrg;
     }
 
-    public Buku26() {
-
+    public int hitungHargaTotal(int jmlJual) {
+        return harga * jmlJual;
     }
 
-    public Buku26(String jud, String pg, int hal, int stok, int har) {
-        judul = jud;
-        pengarang = pg;
-        halaman = hal;
-        this.stok = stok;
-        harga = har;
+    public int hitungDiskon(int hargaTotal) {
+        if (hargaTotal > 150000) { // diskon 12%
+            return (int) (hargaTotal * 0.12);
+        } else if (hargaTotal >= 75000 && hargaTotal <= 150000) {
+            return (int) (hargaTotal * 0.05);
+        } else {
+            return 0;
+        }
+    }
+
+    public int hitungHargaBayar(int hargaTotal) {
+        return hargaTotal -= hitungDiskon(hargaTotal);
     }
 }
--- PrakASD_1F_26/src/P2/SistemBuku/BukuMain26.java
+++ PrakASD_1F_26/src/P2/SistemBuku/BukuMain26.java
@@ -9,15 +9,18 @@ public class BukuMain26 {
         bk1.halaman = 198;
         bk1.stok = 17;
         bk1.harga = 71000;
-
         bk1.tampilkanInformasi();
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilkanInformasi();
 
         Buku26 bk2 = new Buku26("Self Reward", "Maheera ayesha", 160, 29, 59000);
-        bk2.terjual(11);
+        int hargaTotal = bk2.hitungHargaTotal(2);
         bk2.tampilkanInformasi();
+        System.out.println("Harga Total    : Rp." + hargaTotal);
+        System.out.println("hargaDiskon    : Rp." + bk2.hitungDiskon(hargaTotal));
+        System.out.println("Perhitungan    : Rp." + hargaTotal + " - Rp." + bk2.hitungDiskon(hargaTotal));
+        System.out.println("hargaBayar     : Rp." + bk2.hitungHargaBayar(hargaTotal));
 
         Buku26 bukuSatrio = new Buku26("99 Untuk Tuhanku", "Emha Ainun Nadjib", 200, 50, 75000);
         bukuSatrio.tampilkanInformasi();
