3c1c5aa079b21bf0d8b515cf8af84929b76d52e0
Satriowish <satriowisnuap@gmail.com>
2024-02-28 11:36:26 +0700

    P2_Percobaan1_1F_26

---
+++ PrakASD_1F_26/src/P2/SistemBuku/Buku26.java
@@ -0,0 +1,29 @@
+package P2.SistemBuku;
+
+public class Buku26 {
+    String judul;
+    String pengarang;
+    int halaman;
+    int stok;
+    int harga;
+
+    void tampilkanInformasi() {
+        System.out.println("Judul : " + judul);
+        System.out.println("Pengarang : " + pengarang);
+        System.out.println("Jumlah halamn : " + halaman);
+        System.out.println("Sisa Stok : " + stok);
+        System.out.println("Harga : Rp." + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+
+    void restock(int jml) {
+        stok = jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
