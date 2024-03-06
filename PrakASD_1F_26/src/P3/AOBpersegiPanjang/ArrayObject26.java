5a1cf6ca210e799fa61ea072e7faf8ac5bde2122
Satriowish <satriowisnuap@gmail.com>
2024-03-06 11:41:04 +0700

    P3_Percobaan1_1F_26

---
+++ PrakASD_1F_26/src/P3/AOBpersegiPanjang/ArrayObject26.java
@@ -0,0 +1,23 @@
+package P3.AOBpersegiPanjang;
+
+public class ArrayObject26 {
+    public static void main(String[] args) {
+        PersegiPanjang26[] ppArray = new PersegiPanjang26[3];
+
+        ppArray[0] = new PersegiPanjang26();
+        ppArray[0].panjang = 110;
+        ppArray[0].lebar = 30;
+
+        ppArray[1] = new PersegiPanjang26();
+        ppArray[1].panjang = 80;
+        ppArray[1].lebar = 40;
+
+        ppArray[2] = new PersegiPanjang26();
+        ppArray[2].panjang = 100;
+        ppArray[2].lebar = 20;
+
+        System.out.println("Persegi Panjang ke-0, panjang: " + ppArray[0].panjang + ", lebar: " + ppArray[0].lebar);
+        System.out.println("Persegi Panjang ke-1, panjang: " + ppArray[1].panjang + ", lebar: " + ppArray[1].lebar);
+        System.out.println("Persegi Panjang ke-2, panjang: " + ppArray[2].panjang + ", lebar: " + ppArray[2].lebar);
+    }
+}
---
+++ PrakASD_1F_26/src/P3/AOBpersegiPanjang/PersegiPanjang26.java
@@ -0,0 +1,6 @@
+package P3.AOBpersegiPanjang;
+
+public class PersegiPanjang26 {
+    public int panjang;
+    public int lebar;
+}
