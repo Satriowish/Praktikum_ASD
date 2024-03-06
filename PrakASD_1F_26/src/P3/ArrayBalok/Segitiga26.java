098ecdf4fe35700f708b0990e1ff2ab58d64f7ab
Satriowish <satriowisnuap@gmail.com>
2024-03-06 14:50:14 +0700

    P3_Percobaan3_Pertanyaan4_1F_26

--- PrakASD_1F_26/src/P3/ArrayBalok/Segitiga26.java
+++ PrakASD_1F_26/src/P3/ArrayBalok/Segitiga26.java
@@ -22,17 +22,15 @@ public class Segitiga26 {
     public static void main(String[] args) {
         Segitiga26[] sgArray = new Segitiga26[4];
 
-        sgArray[0] = new Segitiga26(15, 5);
+        sgArray[0] = new Segitiga26(10, 4);
         sgArray[1] = new Segitiga26(20, 10);
-        sgArray[2] = new Segitiga26(15, 10);
+        sgArray[2] = new Segitiga26(15, 6);
         sgArray[3] = new Segitiga26(25, 10);
 
         for (int i = 0; i < sgArray.length; i++) {
             System.out.println("Segitiga ke-" + (i + 1) + ":");
             System.out.println("Alas: " + sgArray[i].alas);
             System.out.println("Tinggi: " + sgArray[i].tinggi);
-            System.out.println("Luas : " + sgArray[i].hitungLuas());
-            System.out.println("Keliling : " + sgArray[i].hitungKeliling());
             System.out.println();
         }
     }
