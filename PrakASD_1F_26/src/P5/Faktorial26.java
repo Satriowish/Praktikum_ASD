fff17abb4fc5f39e8117958b694bc7296d578c8d
Satriowish <satriowisnuap@gmail.com>
2024-03-20 11:42:10 +0700

    P5_Percobaan1_Pertanyaan2_1F_26

--- PrakASD_1F_26/src/P5/Faktorial26.java
+++ PrakASD_1F_26/src/P5/Faktorial26.java
@@ -5,8 +5,10 @@ public class Faktorial26 {
 
     int faktorialBF(int n) {
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
+        int i = 1;
+        while (i <= n) {
             fakto = fakto * i;
+            i++;
         }
         return fakto;
     }
