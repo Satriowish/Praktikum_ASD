6a7b141e39e14302ba40422a7e9c9138a3d2ba3a
Satriowish <satriowisnuap@gmail.com>
2024-05-08 11:40:09 +0700

    P10_Praktikum 1_1F_26

--- PrakASD_1F_26/src/UTS_26_1F_SatrioWisnuAdiPratama/ServiceClass26.java
+++ PrakASD_1F_26/src/UTS_26_1F_SatrioWisnuAdiPratama/ServiceClass26.java
@@ -5,20 +5,18 @@ public class ServiceClass26 {
     String namaPembeli;
     String tanggalPembelian;
     int qty;
-    Barang26 brg;
-
-    public int FindBinarySearchStringNama(String cari, int left, int right) {
-        if (right >= left) {
-            int mid = left + (right - left) / 2;
-            if (listBarang26[mid].judulBuku.equalsIgnoreCase(cari)) {
-                return mid;
-            }
-            int compareResult = listBarang26[mid].judulBuku.compareToIgnoreCase(cari);
-            if (compareResult > 0) {
-                return FindBinarySearchStringJudul(cari, left, mid - 1);
-            }
-            return FindBinarySearchStringJudul(cari, mid + 1, right);
-        }
-        return -1;
-    }
+    // public int FindBinarySearchStringNama(String cari, int left, int right) {
+    // if (right >= left) {
+    // int mid = left + (right - left) / 2;
+    // if (listBarang26[mid].judulBuku.equalsIgnoreCase(cari)) {
+    // return mid;
+    // }
+    // int compareResult = listBarang26[mid].judulBuku.compareToIgnoreCase(cari);
+    // if (compareResult > 0) {
+    // return FindBinarySearchStringJudul(cari, left, mid - 1);
+    // }
+    // return FindBinarySearchStringJudul(cari, mid + 1, right);
+    // }
+    // return -1;
+    // }
 }
