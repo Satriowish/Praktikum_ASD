c34ee0b00155afcac8be7ab42d780020610f40bf
Satriowish <satriowisnuap@gmail.com>
2024-05-19 14:53:38 +0700

    P12_Percobaan 3_1F_26

--- PrakASD_1F_26/src/P12/DoubleLinkedList/DLLMain26.java
+++ PrakASD_1F_26/src/P12/DoubleLinkedList/DLLMain26.java
@@ -60,5 +60,35 @@ public class DLLMain26 {
 
         dll.print();
         System.out.println("Size : " + dll.size());
+
+        // Percobaan 3
+        System.out.println();
+        System.out.println("*******************************");
+        System.out.println();
+
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("===============================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("===============================");
+        try {
+            dll.add(40, 1);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("===============================");
+        try {
+            System.out.println("Data awal pada Linked Lists adalah : " + dll.getFirst());
+            System.out.println("Data akhir pada Linked Lists adalah : " + dll.getLast());
+            System.out.println("Data indeks ke-1 pada Linked Lists adalah : " + dll.get(1));
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        }
     }
 }
--- PrakASD_1F_26/src/P12/DoubleLinkedList/DoubleLinkedList26.java
+++ PrakASD_1F_26/src/P12/DoubleLinkedList/DoubleLinkedList26.java
@@ -140,4 +140,33 @@ public class DoubleLinkedList26 {
             size--;
         }
     }
+
+    public int getFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List kosong");
+        }
+        return head.data;
+    }
+
+    public int getLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List kosong");
+        }
+        Node26 tmp = head;
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    public int get(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas.");
+        }
+        Node26 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
 }
