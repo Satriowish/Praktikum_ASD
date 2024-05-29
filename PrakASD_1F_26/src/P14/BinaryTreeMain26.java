6ee728b6751d2686646656ac6c4f5f7b9cf56536
Satriowish <satriowisnuap@gmail.com>
2024-05-29 12:57:04 +0700

    P14_Percobaan 2_1F_26

--- PrakASD_1F_26/src/P14/BinaryTreeMain26.java
+++ PrakASD_1F_26/src/P14/BinaryTreeMain26.java
@@ -28,5 +28,13 @@ public class BinaryTreeMain26 {
         System.out.print("PreOrder Traversal  : ");
         bt.traversePreOrder(bt.root);
         System.out.println("");
+        // **********************************************************
+        BinaryTreeArray26 bta = new BinaryTreeArray26();
+        int[] data = { 6, 4, 8, 3, 5, 7, 9, 0, 0, 0 };
+        int idxLast = 6;
+        bta.populateData(data, idxLast);
+        System.out.print("\nInOrder Traversal   : ");
+        bta.traverseInOrder(0);
+        System.out.println("\n");
     }
 }
