ca1664b85ae90b223faaa12eacbf1147caddd8f4
Satriowish <satriowisnuap@gmail.com>
2024-05-30 19:49:07 +0700

    P14_Tugas_Soal 5_1F_26

--- PrakASD_1F_26/src/P14/Praktikum2/BinaryTreeArray26.java
+++ PrakASD_1F_26/src/P14/Praktikum2/BinaryTreeArray26.java
@@ -21,4 +21,46 @@ public class BinaryTreeArray26 {
         }
     }
 
+    // Tugas soal No 5
+    void traversePreOrder(int idxStart) {
+        if (idxStart <= idxLast && data[idxStart] != 0) {
+            System.out.print(data[idxStart] + " ");
+            traversePreOrder(2 * idxStart + 1);
+            traversePreOrder(2 * idxStart + 2);
+        }
+    }
+
+    void traversePostOrder(int idxStart) {
+        if (idxStart <= idxLast && data[idxStart] != 0) {
+            traversePostOrder(2 * idxStart + 1);
+            traversePostOrder(2 * idxStart + 2);
+            System.out.print(data[idxStart] + " ");
+        }
+    }
+
+    void add(int newData) {
+        if (idxLast == data.length - 1) {
+            System.out.println("Tree is full!");
+            return;
+        }
+
+        if (idxLast == -1) {
+            data[0] = newData;
+            idxLast = 0;
+        } else {
+            idxLast++;
+            data[idxLast] = newData;
+            int current = idxLast;
+
+            while (current > 0) {
+                int parent = (current - 1) / 2;
+                if (data[current] < data[parent]) {
+                    int temp = data[current];
+                    data[current] = data[parent];
+                    data[parent] = temp;
+                }
+                current = parent;
+            }
+        }
+    }
 }
--- PrakASD_1F_26/src/P14/Praktikum2/BinaryTreeArrayMain26.java
+++ PrakASD_1F_26/src/P14/Praktikum2/BinaryTreeArrayMain26.java
@@ -6,8 +6,22 @@ public class BinaryTreeArrayMain26 {
         int[] data = { 6, 4, 8, 3, 5, 7, 9, 0, 0, 0 };
         int idxLast = 6;
         bta.populateData(data, idxLast);
-        System.out.print("\nInOrder Traversal : ");
+
+        System.out.print("\nInOrder Traversal   : ");
         bta.traverseInOrder(0);
+        System.out.print("\nPreOrder Traversal  : ");
+        bta.traversePreOrder(0);
+        System.out.print("\nPostOrder Traversal : ");
+        bta.traversePostOrder(0);
+        System.out.println("\n");
+        bta.add(10);
+        System.out.print("\nContoh Setelah Penambahan Data 10 ");
+        System.out.print("\nInOrder Traversal   : ");
+        bta.traverseInOrder(0);
+        System.out.print("\nPreOrder Traversal  : ");
+        bta.traversePreOrder(0);
+        System.out.print("\nPostOrder Traversal : ");
+        bta.traversePostOrder(0);
         System.out.println("\n");
 
     }
