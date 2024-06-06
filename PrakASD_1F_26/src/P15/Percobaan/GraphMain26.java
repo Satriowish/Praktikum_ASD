3e25776dd4079ba62b99ed090123d3213d68c868
Satriowish <satriowisnuap@gmail.com>
2024-06-06 09:20:03 +0700

    P15_Percobaan 2_1F_26

--- PrakASD_1F_26/src/P15/Percobaan/GraphMain26.java
+++ PrakASD_1F_26/src/P15/Percobaan/GraphMain26.java
@@ -4,25 +4,37 @@ import java.util.Scanner;
 
 public class GraphMain26 {
     public static void main(String[] args) throws Exception {
-        Graph26 gedung = new Graph26(6);
-        gedung.addEdge(0, 1, 50);
-        gedung.addEdge(0, 2, 100);
-        gedung.addEdge(1, 3, 70);
-        gedung.addEdge(2, 3, 40);
-        gedung.addEdge(3, 4, 60);
-        gedung.addEdge(4, 5, 80);
-        gedung.degree(0);
-        gedung.printGraph();
-        gedung.removeEdge(1, 3);
-        gedung.printGraph();
+        // Graph26 gedung = new Graph26(6);
+        // gedung.addEdge(0, 1, 50);
+        // gedung.addEdge(0, 2, 100);
+        // gedung.addEdge(1, 3, 70);
+        // gedung.addEdge(2, 3, 40);
+        // gedung.addEdge(3, 4, 60);
+        // gedung.addEdge(4, 5, 80);
+        // gedung.degree(0);
+        // gedung.printGraph();
+        // gedung.removeEdge(1, 3);
+        // gedung.printGraph();
 
-        // Percobaan 1 Soal No 5
-        Scanner sc26 = new Scanner(System.in);
-        System.out.print("Masukkan gedung asal   : ");
-        int asal = sc26.nextInt();
-        System.out.print("Masukkan gedung tujuan : ");
-        int tujuan = sc26.nextInt();
-        gedung.tetangga(asal, tujuan);
-        sc26.close();
+        // ********* Percobaan 1 Soal No 5 **************
+        // Scanner sc26 = new Scanner(System.in);
+        // System.out.print("Masukkan gedung asal : ");
+        // int asal = sc26.nextInt();
+        // System.out.print("Masukkan gedung tujuan : ");
+        // int tujuan = sc26.nextInt();
+        // gedung.tetangga(asal, tujuan);
+        // sc26.close();
+
+        GraphMatriks26 gdg = new GraphMatriks26(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
     }
 }
---
+++ PrakASD_1F_26/src/P15/Percobaan/GraphMatriks26.java
@@ -0,0 +1,32 @@
+package P15.Percobaan;
+
+public class GraphMatriks26 {
+    int vertex;
+    int[][] matriks;
+
+    public GraphMatriks26(int v) {
+        vertex = v;
+        matriks = new int[v][v];
+    }
+
+    public void makeEdge(int asal, int tujuan, int jarak) {
+        matriks[asal][tujuan] = jarak;
+    }
+
+    public void removeEdge(int asal, int tujuan) {
+        matriks[asal][tujuan] = -1;
+    }
+
+    public void printGraph() throws Exception {
+        for (int i = 0; i < vertex; i++) {
+            System.out.print("Gedung " + (char) ('A' + i) + " : ");
+            for (int j = 0; j < vertex; j++) {
+                if (matriks[i][j] != 1) {
+                    System.out.print("Gedung " + (char) ('A' + j) + " (" + matriks[i][j] + " m),");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
